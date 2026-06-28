4597e2d95aa92957ce10156b8198e0eed8187237
Afitu102 <joxchange@gmail.com>
2026-06-28 19:06:02 +0100

    app-v2.js

--- app-v2.js
+++ app-v2.js
@@ -1,1 +1,14 @@
+// ======================================
+// STORYTIME APP V2
+// One JavaScript for all pages
+// ======================================
 
+// Global Variables
+let currentAudio = null;
+let currentStory = null;
+
+// Detect current page
+const currentPage =
+window.location.pathname
+.split("/")
+.pop();
