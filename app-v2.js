f1aaf2a77bc6ef16d364a94a610b4f512da7e0cd
Afitu102 <joxchange@gmail.com>
2026-06-28 19:08:35 +0100

    app-v2.js

--- app-v2.js
+++ app-v2.js
@@ -12,3 +12,47 @@ const currentPage =
 window.location.pathname
 .split("/")
 .pop();
+
+// ======================================
+// THEME MANAGER
+// ======================================
+
+function loadTheme() {
+
+    const savedTheme =
+        localStorage.getItem("theme") || "dark";
+
+    document.body.setAttribute(
+        "data-theme",
+        savedTheme
+    );
+
+}
+
+function toggleTheme() {
+
+    const currentTheme =
+        document.body.getAttribute("data-theme");
+
+    const newTheme =
+        currentTheme === "dark"
+        ? "light"
+        : "dark";
+
+    document.body.setAttribute(
+        "data-theme",
+        newTheme
+    );
+
+    localStorage.setItem(
+        "theme",
+        newTheme
+    );
+
+}
+
+document.addEventListener(
+    "DOMContentLoaded",
+    loadTheme
+);
+
